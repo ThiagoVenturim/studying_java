175b8ed204c2259fca89ca1b8e1cea16c72aaa0f
Thiago Domingos Venturim Ribeiro dos Santos <thiagodomingosventutim@gmail.com>
2025-07-10 16:09:20 -0300

    Minha Primeira Classe em Java Dog

---
+++ MyFistClass.java
@@ -0,0 +1,17 @@
+class Dog{
+    int size;
+    String breed;
+    String name;  //variaveis de instancia
+
+    void bark(){  
+        System.out.println("Ruff! Ruff!"); // metodo
+    }
+}
+
+class DogTestDrive{ // classe de teste
+    public static void main(String[] args){
+        Dog d = new Dog(); //cria um objeto Dog
+        d.size= 40; //atribui valor a variavel de instancia
+        d.bark(); //chama o metodo bark
+    }
+}
